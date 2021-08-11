80c9b5db8ac2580d775c9e51dd957f6660d37fe6
GZakhar88 <g.zakhar@gmail.com>
2021-08-11 15:06:07 +0200

    CRUD implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,9 +15,49 @@ app.get("/", (req, res) => {
   res.send("My Histamine App");
 });
 
-app.get("/ingredients", async (req, res) => {
+app.get("/ingredients/all", async (req, res) => {
   try {
-    const result = await ingredients.findingredientByName(req.query.name);
+    const result = await ingredients.allIngredients();
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/ingredients/:name", async (req, res) => {
+  try {
+    const result = await ingredients.findingredientByName(req.params.name);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.post("/ingredients", async (req, res) => {
+  try {
+    const result = await ingredients.addIngredient(req.body);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.put("/ingredients", async (req, res) => {
+  try {
+    const result = await ingredients.updateIngredient(req.body);
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete("/ingredients", async (req, res) => {
+  try {
+    const result = await ingredients.deleteIngredient(req.body);
     res.status(200).json(result);
   } catch (error) {
     console.log(error);
--- backend/src/models/ingredients.js
+++ backend/src/models/ingredients.js
@@ -1,12 +1,45 @@
 import { db } from "../data/connection";
 
 export const ingredients = {
+  async allIngredients() {
+    const allIngredient = await db.query("SELECT * FROM ingredient;");
+    return allIngredient.results;
+  },
   async findingredientByName(input) {
     const ingredient = await db.query(
       "SELECT * FROM ingredient WHERE name = ?",
       [input]
     );
-    // console.log("INGREDIENT: " + ingredient);
     return ingredient.results;
+  },
+  async addIngredient(input) {
+    const addNewIngredient = await db.query(
+      "INSERT INTO ingredient (name, tolerancelevel) VALUES (?, ?);",
+      [input.name, input.tolerancelevel]
+    );
+    const newId = await addNewIngredient.results.insertId;
+    const newIngredient = await db.query(
+      "SELECT * FROM ingredient WHERE id = ?;",
+      [newId]
+    );
+    return newIngredient.results;
+  },
+  async updateIngredient(input) {
+    const update = await db.query(
+      "UPDATE ingredient SET name = ?, tolerancelevel = ? WHERE id = ?;",
+      [input.name, input.tolerancelevel, input.id]
+    );
+    const updatedIngredient = await db.query(
+      "SELECT * FROM ingredient WHERE id = ?;",
+      [input.id]
+    );
+    return updatedIngredient.results;
+  },
+  async deleteIngredient(input) {
+    const deleteIngredient = await db.query(
+      "DELETE FROM ingredient WHERE id = ?;",
+      [input.id]
+    );
+    return deleteIngredient.results;
   },
 };

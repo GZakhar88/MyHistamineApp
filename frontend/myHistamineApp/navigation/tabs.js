200aee18ba6ed67a67b7a9910a4abd093ad9f3c0
GZakhar88 <g.zakhar@gmail.com>
2021-08-25 11:31:57 +0200

    icons added to bottom tab nav bar

--- frontend/myHistamineApp/navigation/tabs.js
+++ frontend/myHistamineApp/navigation/tabs.js
@@ -2,25 +2,37 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import "react-native-gesture-handler";
 import addScreen from "../screens/addScreen";
-import editScreen from "../screens/editScreen";
 import listAllScreen from "../screens/listAllScreen";
 import searchScreen from "../screens/searchScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: "#34eba8",
-      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Add a new Item") {
+            iconName = focused ? "add-circle" : "add-circle-outline";
+          } else if (route.name === "Search") {
+            iconName = focused ? "search" : "search";
+          } else if (route.name === "All ingredient") {
+            iconName = focused ? "list" : "list";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
     >
       <Tab.Screen
         name="Add a new Item"
         component={addScreen}
         options={{ unmountOnBlur: true }}
       />
-      {/*  <Tab.Screen name="Edit" component={editScreen} /> */}
       <Tab.Screen
         name="Search"
         component={searchScreen}
